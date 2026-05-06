f5129ad5a44f16bfb62f3831e8075d82fe5a80bb
Mohamed Oussama Belalia <ikram.accounting.19@protonmail.com>
2026-05-06 23:55:17 +0100

    Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,20 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.matax.app',
+  appName: 'Matax',
+  webDir: 'out',
+  server: {
+    url: 'https://glistening-lolly-c73284.netlify.app', 
+    cleartext: true
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#1e3a8a", // Matax Blue
+      showSpinner: true,
+    },
+  }
+};
+
+export default config;
